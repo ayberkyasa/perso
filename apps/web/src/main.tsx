d681886ef1eeeb36a24396615e0a79542aa6b26d
Ayberk Yaşa <ayberkyasa@gmail.com>
2026-07-02 17:22:02 +0200

    feat(web): add router with authentication guards

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,13 +2,13 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 
 import "./index.css"
-import App from "./App.tsx"
-import { ThemeProvider } from "@/components/theme-provider.tsx"
+import { ThemeProvider } from "@/components/theme-provider"
+import { AppRouter } from "@/router"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
-      <App />
+      <AppRouter />
     </ThemeProvider>
   </StrictMode>
 )
